c61be3152b42649ae6fd8cfc0088a030b61f20dd
pablosaavedra334 <pablosaavedra334@gmail.com>
2022-11-15 21:27:06 -0400

    ter

---
+++ Ex_02/Ex_02/Controllers/PlantasController.cs
@@ -0,0 +1,27 @@
+﻿using Ex_02.Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace Ex_02.Controllers
+{
+    public class PlantasController : Controller
+    {
+        private readonly IRepository repository;
+
+        public PlantasController(IRepository repository)
+        {
+
+            this.repository = repository;
+
+
+        }
+
+        public IActionResult Index() //Index/agregar vista/vista razor / (index/list/Product..p2../)
+        {
+
+            return View(this.repository.GetProducts());
+        }
+
+
+    }
+}
