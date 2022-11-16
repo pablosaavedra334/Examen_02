eaa7f05917063b714389aa53003e3ae62264554d
pablosaavedra334 <pablosaavedra334@gmail.com>
2022-11-15 21:53:47 -0400

    ultimaa

--- Ex_02/Ex_02/Controllers/PlantasController.cs
+++ Ex_02/Ex_02/Controllers/PlantasController.cs
@@ -1,4 +1,5 @@
-﻿using Ex_02.Data.Repository;
+﻿using Ex_02.Data.Entity;
+using Ex_02.Data.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Client;
 
@@ -21,6 +22,47 @@ namespace Ex_02.Controllers
 
             return View(this.repository.GetProducts());
 
+
+        }
+        public IActionResult Details(int? Id) //Index/agregar vista/vista razor / (index/Details/Product..p2../)
+        {
+
+            if (Id == null)
+            {
+                return NotFound();
+
+            }
+
+            var product = this.repository.GetProduct(Id.Value);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+
+
+
+        }
+
+        [HttpGet]   
+        public IActionResult Create()  
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Plantas product) 
+        {
+            if (ModelState.IsValid)
+            {
+                this.repository.AddProduct(product);
+                await this.repository.SaveAllAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(product);
 
         }
 
